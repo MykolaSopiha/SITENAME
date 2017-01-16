4f34dfc3595967edf9a890bd0db35a1b94ca84d6
Mykola Sopiha <niksop94@gmail.com>
2017-01-16 03:20:23 +0200

    first commit

---
+++ script/js.js
@@ -0,0 +1,35 @@
+// Scroll Up!
+$(document).ready(function(){
+    
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+    
+});
+
+$(document).ready(function(){
+	var touch = $('#touch-menu');
+    var menu = $('.nav');
+ 
+    $(touch).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+    $(window).resize(function(){
+        var wid = $(window).width();
+        if(wid > 760 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+});
